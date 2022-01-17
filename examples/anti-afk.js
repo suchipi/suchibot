a1543a380cd80a062b7fc1bc7df98df774c151a2
Lily Scott <git@suchipi.tv>
2022-01-16 22:31:41 -0700

    Add some examples

---
+++ examples/anti-afk.js
@@ -0,0 +1,16 @@
+import { Keyboard, Key, sleep } from "../index";
+
+// This script presses "A" or "D" every 5 minutes,
+// To prevent your character from getting kicked from a game for being AFK.
+
+// DELAY is in milliseconds, and there's 60 seconds in a minute, and we want a delay of 5 minutes
+const DELAY = 5 * 60 * 1000;
+
+(async () => {
+  while (true) {
+    Keyboard.tap(Key.A);
+    await sleep(DELAY);
+    Keyboard.tap(Key.D);
+    await sleep(DELAY);
+  }
+})();
---
+++ examples/toggle-holding-left-click.js
@@ -0,0 +1,16 @@
+import { Keyboard, Key, Mouse, MouseButton } from "../index";
+
+// This script toggles whether the left mouse button is being held whenever you press scroll lock.
+// I use it to hold down left-click in Minecraft so I can keep mining cobblestone from the cobblestone generator :3
+
+let shouldHold = false;
+
+Keyboard.onUp(Key.SCROLL_LOCK, () => {
+  shouldHold = !shouldHold;
+
+  if (shouldHold) {
+    Mouse.hold(MouseButton.LEFT);
+  } else {
+    Mouse.release(MouseButton.LEFT);
+  }
+});
